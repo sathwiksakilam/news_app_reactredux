51e0e7a2c55442d2d5e0a180fb8ccc8f68e3eae5
Your Name <you@example.com>
2024-06-20 16:21:36 +0530

    Removed composewithdevtools

--- src/store.js
+++ src/store.js
@@ -1,11 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './redux/rootReducer';
-// import logger from 'redux-logger';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import {thunk} from 'redux-thunk';  // Import 'thunk' as a default export
 
-const store = createStore(rootReducer, composeWithDevTools(
-    applyMiddleware(thunk)  
-));
+// Create store with middleware applied directly
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+);
 
 export default store;
